2a1833cfa984ff1e34fa54ab01d8611ee98d8886
Isaac <isaacluiz0808@gmail.com>
2024-09-03 13:34:53 -0300

    Adicionando um servidor na porta 8100

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import 'graphql-import-node';
 import { execute, parse } from "graphql";
 import { schema } from "./schema";
+import fastify = require('fastify');
 
 async function main() {
-  const myQuery = parse(`query { info }`);
-
-  const result = await execute({
-    schema,
-    document: myQuery,
+  const server = fastify();
+  
+  server.get("/", (req, reply) => {
+    reply.send({ test: true });
   });
 
-  console.log(result);
+  server.listen(8100,"0.0.0.0", () => {
+    console.log(`Server is running on http://localhost:8100`);
+  });
 }
 
 main();
