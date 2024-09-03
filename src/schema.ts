76e11a74a841e49e7f104b3f842c05dfa3f6f04f
Isaac <isaacluiz0808@gmail.com>
2024-09-03 17:44:54 -0300

    Adicionando type Categoria { id:     ID! name:   String! } no schema.graphql e no shcema.ts: type Categoria = { id:     string; name:   string; } const categorias: Categoria[] = [ {id: 'categoria-0', name: 'arroz'} ], juntamente com a sua mutation para criar uma nova categoria: Mutation: { createUser: (_: any, {name}: {name: string}) => { const newUser: User = { id: user-, name }; users.push(newUser); return newUser; }, createCategoria: (_: any, { name }: { name: string }) => { const newCategoria: Categoria = { id: categoria-, name }; categorias.push(newCategoria); return newCategoria; } }, também foi enviada nesse commit a mutation completa, que abrange também a criação de um novo usuário, para que o commit possa ser completo!!

--- src/schema.ts
+++ src/schema.ts
@@ -20,20 +20,34 @@ type User = {
 
 const users: User[] = [
     {id: 'user-0', name: 'joao'},
-    {id: 'user-1', name: 'filipi'   }  
+    {id: 'user-1', name: 'filipi'}  
 ];
+
+type Categoria = {
+    id:     string;
+    name:   string;
+}
+
+const categorias: Categoria[] = [
+    {id: 'categoria-0', name: 'arroz'}
+]
 
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
         feed: () => links,
         users: () => users,
+        categorias: () => categorias,
     },
     Link: {
         id: (parent: Link) => parent.id,
         description: (parent: Link) => parent.description,
         url: (parent: Link) => parent.url,
       },
+    Categoria: {
+        id: (parent: Categoria) => parent.id,
+        name: (parent: Categoria) => parent.name,
+    },
     Mutation: {
         createUser: (_: any, {name}: {name: string}) => {
             const newUser: User = {
@@ -42,6 +56,14 @@ const resolvers = {
             };
             users.push(newUser);
             return newUser;
+        },
+        createCategoria: (_: any, { name }: { name: string }) => {
+            const newCategoria: Categoria = {
+                id: `categoria-${categorias.length}`,
+                name
+            };
+            categorias.push(newCategoria);
+            return newCategoria;
         }
     }
 }
